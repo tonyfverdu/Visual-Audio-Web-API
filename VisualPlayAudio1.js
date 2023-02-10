300b46482094fdf5024b931e2585919608f0c32f
tonyfverdu <tony_verdu@hotmail.com>
2023-02-11 00:43:46 +0100

    new buttons

--- VisualPlayAudio1.js
+++ VisualPlayAudio1.js
@@ -1,32 +1,47 @@
 //  A.-  Definition init of system
 //  0.-  AudioContext of System
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
 //  1.- HTMLElement (fileSong, audioElement, canvasElement, playPauseButton, seekbar and volumenBar)
 const songSelect = document.querySelector('.selectSong')
 const optionElement = document.querySelectorAll('option')
 const buttonSelectSong = document.querySelector('.buttonSelect')
-// const fileSong = document.querySelector("#songChose")
 let fileSong = "./assets/mp3/John Lennon 'Imagine'.mp3"
-const audioElement = document.querySelector('.audioController');
-const canvasElement = document.querySelector('.canvas');
-const playPauseButton = document.querySelector('.playPause');
-const seekBar = document.querySelector('.seekbar');
-const volumenBar = document.querySelector('.volume');
+const audio = new Audio(fileSong);
+let durationMinuten
+audio.preload = "auto"
+const canvasElement = document.querySelector('.canvas')
+const playPauseButton = document.querySelector('.playPause')
+const seekBar = document.querySelector('.seekBar')
+const iniSeekBar = document.querySelector('.IniSeekBar')
+iniSeekBar.innerHTML = `0:00m`
+const endSeekBar = document.querySelector('.EndSeekBar')
+const currentSeekBar = document.querySelector('.CurrentSeekBar')
+const volumenBar = document.querySelector('.volume')
+const mutedVolume = document.querySelector(".mutedButton")
+const volumeDown = document.querySelector("#buttonDown")
+const volumeUp = document.querySelector("#buttonUp")
+
+audio.addEventListener("loadeddata", function() {
+    durationMinuten = (this.duration / 60).toFixed(2);
+    endSeekBar.innerHTML = `${durationMinuten}m`
+   })
 
 //  2.- Canvas configuration
-const canvasCtx = canvasElement.getContext('2d');
-const WIDTH = canvasElement.clientWidth;
-const HEIGHT = canvasElement.clientHeight;
+const canvasCtx = canvasElement.getContext('2d')
+const WIDTH = canvasElement.clientWidth
+const HEIGHT = canvasElement.clientHeight
 
 //  3.- Bars (seekbar and volumenBar) init configuration
-seekBar.value = 0;
-volumenBar.value = 50;
+seekBar.value = 0
+volumenBar.value = 50
+audio.volume = 0.5
+mutedVolume.value = false
 
 //  4.-  Messages of button (playPause)
-const pauseIcon = `<span class="materialIcons">Pause</span>`;
-const playIcon = `<span class="materialIcons">Play</span>`;
-const replayIcon = `<span class="materialIcons">Replay</span>`;
+const pauseIcon = `<span class="material-symbols-outlined pause">pause_circle</span>`;
+const playIcon = `<span class="material-symbols-outlined play">play_circle</span>`;
+const replayIcon = `<span class="material-symbols-outlined replay">replay</span>`;
 
 //  5.-  Definition of variable "audioState", 2 values: replay oder paused
 let audioState = {
@@ -41,23 +56,23 @@ function togglePlayPause() {
     audioCtx.resume().then(() => {
         if (audioState.isPaused) {
             playPauseButton.innerHTML = pauseIcon;
-            audioElement.play();
+            audio.play();
         } else {
             if (audioState.isReplay) { // Replay
                 playPauseButton.innerHTML = pauseIcon;
-                audioElement.play();
+                audio.play();
                 audioState.isReplay = false;
                 return;
             }
             playPauseButton.innerHTML = playIcon;
-            audioElement.pause();
+            audio.pause();
         }
         audioState.isPaused = !audioState.isPaused;
     });
 }
 
 //  2.-  Event "change" in the input type="file" (fileSong) (change => ev.target.files[0].name --> src of audioElement)
-audioElement.setAttribute('src', `./assets/mp3/John Lennon 'Imagine'.mp3`)
+audio.setAttribute('src', `./assets/mp3/John Lennon 'Imagine'.mp3`)
 // fileSong.addEventListener('change', (ev) => {
 //     if (ev.target.files[0]) {
 //         audioElement.setAttribute('src', `./assets/mp3/${ev.target.files[0].name}`);
@@ -69,43 +84,80 @@ songSelect.addEventListener('change', (ev) => {
 })
 
 buttonSelectSong.addEventListener('click', () => {
-    audioElement.setAttribute('src', fileSong);
+    audio.setAttribute('src', fileSong);
 })
 
 //  3.- Events in the audioElement (timeupdate, ended and canplay)
 //  3.1.- Event "timeupdate" in the audioElement (timeupdate ==> setProgress)
-audioElement.addEventListener('timeupdate', setProgress);
+audio.addEventListener('timeupdate', setProgress);
+
 function setProgress() {
-    seekBar.value = audioElement.currentTime;
+    currentSeekBar.innerHTML = `${ ((100 * audio.currentTime) / (durationMinuten * 60)).toFixed(2) }%`;
+    seekBar.value = audio.currentTime;
+
 }
 //  3.2.- Event "ended" in the audioElement (ended==> onEnd).  End of play music in the audioElement
-audioElement.addEventListener('ended', onEnd);
+audio.addEventListener('ended', onEnd);
 function onEnd() {
     playPauseButton.innerHTML = replayIcon;
-    audioElement.currentTime = 0;
+    audio.currentTime = 0;
     seekBar.value = 0;
     audioState.isReplay = true;
 }
 //  3.3.- Event "canplay" in the audioElement (canplay ==> setDuration)
-audioElement.addEventListener('canplay', setDuration);
+audio.addEventListener('canplay', setDuration);
 function setDuration() {
-    seekBar.max = audioElement.duration;
+    seekBar.max = audio.duration;
 }
+//  3.4.- Event "muted" in the mutedVolume (muted => !muted)
+const mutedIcon = '<span class="material-symbols-outlined volume_muted">volume_off</span>'
+const notMutedIcon =  '<span class="material-symbols-outlined volume_Notmuted">volume_up</span>'
+
+mutedVolume.addEventListener('click', () => {
+    if(mutedVolume.value === "false") {
+        mutedVolume.innerHTML = mutedIcon;
+        mutedVolume.value = "true"
+        audio.muted = true
+    } else {
+        mutedVolume.innerHTML = notMutedIcon;
+        mutedVolume.value = "false"
+        audio.muted = false
+    }
+})
+
+//  3.5- Events "change volume" in the buttonVolume 
+volumeDown.addEventListener('click', () => {
+    if (audio.volume >= 0.10) {
+        audio.volume =  audio.volume - 0.1;
+    } else {
+        audio.volume =  0;
+    }
+    volumenBar.value =  audio.volume * 100
+})
+volumeUp.addEventListener('click', () => {
+    if (audio.volume <= 0.9) {
+        audio.volume =  audio.volume + 0.1;
+    } else {
+        audio.volume =  1;
+    }
+    volumenBar.value =  audio.volume * 100
+})
+
 // 4.-  Event "input" in the bar (input type "range) "seekBar", (input => onSeek)
 seekBar.addEventListener('input', onSeek);
 function onSeek(evt) {
-    audioElement.currentTime = evt.target.value;
+    audio.currentTime = evt.target.value;
 }
 
 // 5.-  Event "input" in the bar (input type "range") "volumenBar", (input => onVolumenSeek)
 volumenBar.addEventListener('input', onVolumenSeek);
 function onVolumenSeek(ev) {
-    audioElement.volume = ev.target.value / 100;
+    audio.volume = ev.target.value / 100;
 }
 
 //  Connect the "audio source" (AudioNode "audioElement") with the context of audio ("audioCtx"), 
 //  so we define the audio source of the audio graph.
-const source = audioCtx.createMediaElementSource(audioElement);
+const source = audioCtx.createMediaElementSource(audio);
 const analyser = audioCtx.createAnalyser();  //  create a analyser of audio
 analyser.fftSize = 2048;  //  Fourier transform
 
@@ -125,18 +177,19 @@ function drawSound() {
     canvasCtx.fillStyle = 'rgb(0, 0, 0)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    const barWidth = (WIDTH / bufferLength) * 2.3;
+    const barWidth = (WIDTH / bufferLength) * 1.1;
     let barHeight;
     let x = 0;
 
     for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i] / 0.82;
+        barHeight = dataArray[i] / 1.1;
 
         //  Colors of bars
-        const c = i * 1.1 / bufferLength;
-        const redColor = 253;
-        const greenColor = 253 * 4 * c;
-        const blueColor = barHeight * 3 * c;
+        const g = i * 1.2 / bufferLength;
+        const b = i * 1.1 / bufferLength;
+        const redColor = 255;
+        const greenColor = 255 * 3 * g;
+        const blueColor = barHeight * 4 * b;
         canvasCtx.fillStyle = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
         canvasCtx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
         x += barWidth + 1;
